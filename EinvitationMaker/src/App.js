10956cba3909e7e27a5470ee2e5fe5bf23f6ba8e
unknown <aswalshrey@gmail.com>
2023-02-16 14:59:15 +0530

    updated einvitationMaker

---
+++ EinvitationMaker/src/App.js
@@ -0,0 +1,19 @@
+// import {Route, Routes} from 'react-router-dom'
+import './App.css';
+import WedAnniv1 from './components/WedAnniv/WedAnniv1/WedAnniv';
+// import Btemp1 from './components/birthday/Btemp1/Btemp1';
+// import Wtemp1 from './components/wedding/Wtemp1/Wtemp1';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Btemp1/> */}
+      {/* <Wtemp1/> */}
+      <WedAnniv1/>
+    </div>
+  );
+}
+
+export default App;

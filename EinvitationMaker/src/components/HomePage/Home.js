b8fc59131302ef2a0c294f8c5f31f0fea86028ad
unknown <aswalshrey@gmail.com>
2023-04-10 23:41:08 +0530

    Hosted Backend, Scroll feature, minor template CSS, credit

--- EinvitationMaker/src/components/HomePage/Home.js
+++ EinvitationMaker/src/components/HomePage/Home.js
@@ -1,30 +1,46 @@
-import React, { useState } from 'react'
-import Navbar from '../Navbar'
-import Greeting from './Greeting'
-import LandingTemplateContainer from './LandingTemplateContainer'
-import '../Combined.css'
-import Loader from '../Loader'
+import React, { useState } from "react";
+import Navbar from "../Navbar";
+import Greeting from "./Greeting";
+import LandingTemplateContainer from "./LandingTemplateContainer";
+import "../Combined.css";
+import Loader from "../Loader";
+import { appContext } from "../AppContext";
+
+// export const appContext = createContext(null);
 
 function Home() {
+  const [load, setLoad] = useState(true);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-  const [load,setLoad] = useState(true)
-
-
+  // console.log(appContext.)
   setTimeout(() => {
-    setLoad(false)
-  },2200)
+    setLoad(false);
+  }, 2200);
+
+
+  const updateSelectedCategory = (category) => {
+    setSelectedCategory(category);
+    // console.log("Selected category:", selectedCategory);
+  };
+
+  // console.log(chosenHeading);
   return (
     <>
-    {
-      load ? <Loader/> : <div className='home'>
-      <Navbar/>
-      <Greeting/>
-      <LandingTemplateContainer/>
-    </div>
-    }
+      {load ? (
+        <Loader />
+      ) : (
+        <appContext.Provider
+          value={{ selectedCategory, updateSelectedCategory }}
+        >
+          <div className="home">
+            <Navbar />
+            <Greeting />
+            <LandingTemplateContainer />
+          </div>
+        </appContext.Provider>
+      )}
     </>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
--- EinvitationMaker/src/components/HomePage/LandingTemplateContainer.js
+++ EinvitationMaker/src/components/HomePage/LandingTemplateContainer.js
@@ -1,64 +1,79 @@
-import React from "react";
+import React, { useContext, useRef, useEffect } from "react";
 import headingData from "../CategoriesData";
 import rightArrow from "../Templates/arrow-right.svg";
 import { useNavigate } from "react-router-dom";
+import { appContext } from "../AppContext";
 
 function TemplateContainer() {
   const navigate = useNavigate();
+  const reference = useRef({});
 
-  console.log(headingData)
+  const { selectedCategory } = useContext(appContext);
+
   const handleOnClickShowMore = (data) => {
-    // console.log(data.id);
-
-    navigate(`/${data.title}`, { state: { title: data.title,id: data.id } });   
-    // const headingCategory = {title: `${data.title}`}
-    // return headingCategory
+    navigate(`/${data.title}`, { state: { title: data.title, id: data.id } });
   };
 
   const handleClick = (data) => {
-    navigate('/editing', {state: {id: data.id, temp: data.template}})
-  }
-  
- 
-  return headingData.map((data, index) => {
-    return (
-      <div className="templatesContainer" key={index}>
-        <div className="topTemplatesContainer">
-          <h1 className="categoryHeadings">{data.title}</h1>
+    navigate("/editing", { state: { id: data.id, temp: data.template } });
+  };
 
-          <div
-            className="showMore_Icon"
-            onClick={() => handleOnClickShowMore(data)}
-          >
-            <h4>Show more</h4>
-            <img src={rightArrow} alt="showMoreIcon" id="showMoreIcon"></img>
+  useEffect(() => {
+    const selectedRef = reference.current[selectedCategory];
+    if (selectedRef) {
+      const y = selectedRef.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo({ behavior: "smooth", top: y - 40 });
+    }
+  }, [selectedCategory]);
+
+  return (
+    <>
+      {headingData.map((data, index) => {
+        console.log(data);
+        return (
+          <div className="templatesContainer" key={index}>
+            <div className="topTemplatesContainer">
+              <h1
+                className="categoryHeadings"
+                ref={(ref) => (reference.current[data.title] = ref )}
+                // id={data.title === selectedCategory ? "selectedHeading" : "not"}
+              >
+                {/* {console.log(reference.current[data?.title])} */}
+                {data.title}
+              </h1>
+              <div
+                className="showMore_Icon"
+                onClick={() => handleOnClickShowMore(data)}
+              >
+                <h4>Show more</h4>
+                <img
+                  src={rightArrow}
+                  alt="showMoreIcon"
+                  id="showMoreIcon"
+                ></img>
+              </div>
+            </div>
+            <div className="bottomTemplatesContainer">
+              <div className="boxesContainer">
+                {data.subHeading.map((subData, index) => {
+                  return (
+                    <img
+                      src={subData.template}
+                      alt="template"
+                      className="TemplateContainer"
+                      key={index}
+                      onClick={() => handleClick(subData)}
+
+                    />
+                  );
+                })}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="bottomTemplatesContainer">
-          <div className="boxesContainer">
-            {data.subHeading.map((subData, index) => {
-              // const jsxc = subData.template
-              return (
-                // <div className="TemplateContainer" key={index}>
-                
-                  <img
-                    src={subData.template}
-                    alt="template"
-                    className="TemplateContainer"
-                    key={index}
-                    // id={data.id}
-                    onClick={() => handleClick(subData)}
-                    
-                    // imgId={subData.id}
-                  />
-                // </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    );
-  });
+        );
+      })}
+    </>
+  );
 }
 
 export default TemplateContainer;
